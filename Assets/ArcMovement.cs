46f67100b1523b68121c578b681707f24a86bf72
gazor117 <oreillygary7@gmail.com>
2019-02-08 17:41:27 +0000

    Basic Shooting Implemented

---
+++ Assets/ArcMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcMovement : MonoBehaviour
+{
+    private Vector3 targetDir;
+    private Vector3 target;
+    private float dist;
+    private float angle;
+    public float velocity;
+
+    public GameObject projectile;
+    public Vector3 targetObj;
+    public float shootAngle;
+    public float bolfSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GameObject thunderBolt = Instantiate(projectile, transform.position, Quaternion.identity);
+            Rigidbody boltRB = thunderBolt.GetComponent<Rigidbody>();
+            boltRB.velocity = BallisticVel(targetObj, shootAngle);
+            
+
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit)
+            {
+                if (hitInfo.transform.gameObject.tag == "Tile")
+                {
+                    targetObj = hitInfo.transform.gameObject.transform.position;
+                }
+            }
+        }
+    
+        
+        
+        
+    }
+
+
+    public Vector3 BallisticVel(Vector3 target, float angle)
+    {
+        targetDir = target - transform.position;            //target direction
+        dist = targetDir.magnitude;
+        float radAngle = angle * Mathf.Deg2Rad;
+        targetDir.y = dist * Mathf.Tan(radAngle);            //set targetDir to elevation angle
+        velocity = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(radAngle * 2));
+        return velocity * targetDir.normalized;
+
+    }
+    
+   
+}
