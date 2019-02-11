0ab55e09bb2d649dfc9142823c08bea533e95a6b
gazor117 <oreillygary7@gmail.com>
2019-02-11 10:14:05 +0000

    Basic 2 Player shooting

--- Assets/Scripts/ArcMovement.cs
+++ Assets/Scripts/ArcMovement.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class ArcMovement : MonoBehaviour
 {
@@ -23,6 +26,11 @@ public class ArcMovement : MonoBehaviour
     private float energyLevel;
 
     public Energy boltEnergyscript;
+
+    public bool player1, player2;
+    
+
+    public Image cursor;
     
     // Start is called before the first frame update
     void Start()
@@ -34,56 +42,108 @@ public class ArcMovement : MonoBehaviour
     void Update()
     {
        // print(energyLevel);
-        if (Input.GetKey(KeyCode.Space))
+        if (player1)
         {
-            ChargeBolt();
+            if (Input.GetKey(KeyCode.Space))
+            {
+                ChargeBolt();
+
+            }
+
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                GameObject thunderBolt = Instantiate(projectile, transform.position, Quaternion.identity);
+                //thunderBolt.transform.localScale += ChargeBolt();
+                Rigidbody boltRB = thunderBolt.GetComponent<Rigidbody>();
+                boltRB.velocity = BallisticVel(targetObj, shootAngle);
+                //boltEnergyscript.energyLevel() += energyLevel;
+                EnergyHolder EH = thunderBolt.AddComponent<EnergyHolder>();
+                RaycastHit checkTile;
+
+                if (Physics.Raycast(transform.position, -transform.up, out checkTile, 1))
+                {
+                    GameObject tileBelow = checkTile.transform.gameObject;
+                    float energyDrained = Mathf.Floor(tileBelow.GetComponent<Energy>().energy * ballCharge);
+                    tileBelow.GetComponent<Energy>().drainEnergy(ballCharge);
+
+                    EH.EnergyLevel(energyDrained);
+                }
+
+
+
+                sizeIncrement = Vector3.zero;
+                ballCharge = 0;
+
+
+
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hitInfo = new RaycastHit();
+                bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+                if (hit)
+                {
+                    if (hitInfo.transform.gameObject.tag == "Tile")
+                    {
+                        targetObj = hitInfo.transform.gameObject.transform.position;
+                    }
+                }
+            }
+
 
         }
-       
-        
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            GameObject thunderBolt = Instantiate(projectile, transform.position, Quaternion.identity);
-            //thunderBolt.transform.localScale += ChargeBolt();
-            Rigidbody boltRB = thunderBolt.GetComponent<Rigidbody>();
-            boltRB.velocity = BallisticVel(targetObj, shootAngle);
-            //boltEnergyscript.energyLevel() += energyLevel;
-            EnergyHolder EH = thunderBolt.AddComponent<EnergyHolder>();
-            RaycastHit checkTile;
 
-            if (Physics.Raycast(transform.position, -transform.up, out checkTile, 1))
+        if (player2)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                GameObject tileBelow = checkTile.transform.gameObject;
-                float energyDrained = Mathf.Floor(tileBelow.GetComponent<Energy>().energy * ballCharge);
-                tileBelow.GetComponent<Energy>().drainEnergy(ballCharge);
-                
-                EH.EnergyLevel(energyDrained);
+                cursor.transform.position += Vector3.right;
+
+
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                cursor.transform.position -= Vector3.right;
+
+
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                cursor.transform.position += Vector3.up;
+
+
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                cursor.transform.position -= Vector3.up;
+
+
             }
             
             
-
-            sizeIncrement = Vector3.zero;
-            ballCharge = 0;
-
             
+            Ray ray2 = Camera.main.ScreenPointToRay(cursor.transform.position);
+            RaycastHit hit2;
 
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hitInfo = new RaycastHit();
-            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
-            if (hit)
+            if (Physics.Raycast(ray2, out hit2, 10000))
             {
-                if (hitInfo.transform.gameObject.tag == "Tile")
+
+                if (hit2.transform.tag == "Player1Tile")
                 {
-                    targetObj = hitInfo.transform.gameObject.transform.position;
+                    if (Input.GetKeyDown(KeyCode.KeypadEnter))
+                    {
+                        print("Tile Selected");
+                        targetObj = hit2.transform.gameObject.transform.position;
+                    }
+
                 }
             }
         }
-    
-        
-       
-        
     }
 
 
@@ -111,6 +171,6 @@ public class ArcMovement : MonoBehaviour
         return velocity * targetDir.normalized;
 
     }
-    
+
    
 }
