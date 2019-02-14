6a508e886bd111b4c2ac2368c06afe923134a168
ConorFT <43806423+ConorFT@users.noreply.github.com>
2019-02-14 17:11:28 +0000

    It's Alive

---
+++ Assets/Scripts/AnimatorParameters.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorParameters : MonoBehaviour
+{
+
+    Movement move_script;
+    Animator anim;
+
+    void Start()
+    {
+        move_script = GetComponent<Movement>();
+        anim = GetComponentInChildren<Animator>();
+    }
+    
+
+    void Update()
+    {
+        if (move_script.X_isAxisInUse == true || move_script.Y_isAxisInUse == true)
+        {
+            anim.SetBool("Is Moving", true);
+        } else
+        {
+            anim.SetBool("Is Moving", false);
+        }
+    }
+}
