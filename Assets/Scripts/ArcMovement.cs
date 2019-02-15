e45de167b3865f43e6d7b4521fa1bbe305ca00d8
gazor117 <oreillygary7@gmail.com>
2019-02-15 02:47:51 +0000

    Skull Islands

--- Assets/Scripts/ArcMovement.cs
+++ Assets/Scripts/ArcMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
-using UnityEditorInternal;
+//using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 using Quaternion = UnityEngine.Quaternion;
